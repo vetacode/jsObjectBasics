826fc37b8c7cc49b0131d601b46e189f89fc5d44
vetacode <vetacode@gmail.com>
2025-11-04 17:44:11 +0700

    adding multiword keys

--- keyValuePairs.js
+++ keyValuePairs.js
@@ -9,3 +9,16 @@ console.log(user);
 
 delete user.age;
 console.log(user);
+
+//multiword keys
+let hero = {
+  name: 'Vetacode',
+  'like coding': true,
+};
+
+//Accessing or Getting multiword keys
+console.log(hero['like coding']);
+
+//deleting
+delete hero['like coding'];
+console.log(hero);
