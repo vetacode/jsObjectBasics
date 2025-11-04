323183af23999ad4a909f56415d261488f9bf2d3
vetacode <vetacode@gmail.com>
2025-11-04 20:36:10 +0700

    adding integer on for in

--- forInLoop.js
+++ forInLoop.js
@@ -24,3 +24,27 @@ console.log(Object.values(user));
 //combining forEach
 Object.keys(user).forEach((key) => console.log(key));
 Object.values(user).forEach((value) => console.log(value));
+
+//integers ordered by ascending
+
+let codes = {
+  49: 'Germany',
+  41: 'Switzerland',
+  44: 'Great Britain',
+  // ..,
+  1: 'USA',
+};
+
+for (let code in codes) {
+  console.log(code); // 1, 41, 44, 49
+}
+
+//The “integer property” term here means a string that can be converted to-and-from an integer without a change.
+
+//So, "49" is an integer property name, because when it’s transformed to an integer number and back, it’s still the same. But "+49" and "1.2" are not:
+
+// Number(...) explicitly converts to a number
+// Math.trunc is a built-in function that removes the decimal part
+console.log(String(Math.trunc(Number('49')))); // "49", same, integer property
+console.log(String(Math.trunc(Number('+49')))); // "49", not same "+49" ⇒ not integer property
+console.log(String(Math.trunc(Number('1.2')))); // "1", not same "1.2" ⇒ not integer property
