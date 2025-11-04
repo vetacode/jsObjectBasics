6e0ef73f2b1d673518c9660c4bed5c25d9a1423a
vetacode <vetacode@gmail.com>
2025-11-04 20:12:38 +0700

    converting to array of for in

---
+++ forInLoop.js
@@ -0,0 +1,24 @@
+/*
+//The syntax:
+for (key in object) {
+  // executes the body for each key among object properties
+}
+*/
+
+let user = {
+  name: 'Xena',
+  age: 23,
+  isAdmin: false,
+};
+
+for (let key in user) {
+  console.log(key); //get all the keys (prop name)
+
+  console.log(user[key]); //get all the value props
+}
+
+//converting to Array
+console.log(Object.keys(user));
+console.log(Object.values(user));
+
+//combining forEach
