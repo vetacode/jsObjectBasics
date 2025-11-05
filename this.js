556a799964da921a68e9dfd2b59d72d36838068c
vetacode <vetacode@gmail.com>
2025-11-05 20:21:32 +0700

    adding method writing method

--- this.js
+++ this.js
@@ -11,7 +11,7 @@ user.sayHi(); // Hello!
 
 console.log(user);
 //A function that is a property of an object is called its method.
-//sayHiß is a function that is a property of user object
+//sayHi is a function that is a property of user object
 //sayHi is a METHOD of the object user.
 
 let mobil = {};
@@ -23,3 +23,23 @@ function terbang() {
 mobil.terbang = terbang; //masukin key terbang ke obj mobil dgn value function terbang ==> terbang adalah method
 
 mobil.terbang();
+
+//Cara penulisan method
+let ukuran = 300;
+
+let rumah = {
+  isHuge: function () {
+    return `${ukuran}m2 sedeng lah ya..`;
+  },
+};
+
+console.log(rumah.isHuge());
+
+//shorthand
+let rumah2 = {
+  isHuge() {
+    return `${ukuran}m2 sedeng lah ya..`;
+  },
+};
+
+console.log(rumah2.isHuge());
