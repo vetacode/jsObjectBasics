a23a9bc4b44079a42e5c4613aea10f864ea4cbe7
vetacode <vetacode@gmail.com>
2025-11-05 13:28:37 +0700

    adding object equality

--- objectReference.js
+++ objectReference.js
@@ -9,3 +9,15 @@ console.log(user);
 admin.name = 'Brandon'; //this is changing the object reference
 
 console.log(user.name); //therefore the user object also changes
+
+//object equality: must reference the same object
+let a = {};
+let b = a; // copy the reference
+
+console.log(a == b); // true, both variables reference the same object
+console.log(a === b); // true
+
+let x = {};
+let y = {}; // two independent objects
+
+console.log(x == y); // false
