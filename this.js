9a12a2bf0cb57bf73f3e454ffb80a263aee47b44
vetacode <vetacode@gmail.com>
2025-11-05 22:45:04 +0700

    task 3 done

--- this.js
+++ this.js
@@ -180,3 +180,53 @@ let calculator = {
 calculator.read();
 console.log(calculator.sum());
 console.log(calculator.mul());
+
+/*Task 3
+Chaining
+importance: 2
+There’s a ladder object that allows you to go up and down:
+
+let ladder = {
+  step: 0,
+  up() {
+    this.step++;
+  },
+  down() {
+    this.step--;
+  },
+  showStep: function() { // shows the current step
+    alert( this.step );
+  }
+};
+Now, if we need to make several calls in sequence, we can do it like this:
+
+ladder.up();
+ladder.up();
+ladder.down();
+ladder.showStep(); // 1
+ladder.down();
+ladder.showStep(); // 0
+Modify the code of up, down, and showStep to make the calls chainable, like this:
+
+ladder.up().up().down().showStep().down().showStep(); // shows 1 then 0
+Such an approach is widely used across JavaScript libraries.
+
+*/
+
+let ladder = {
+  step: 0,
+  up() {
+    this.step++;
+    return this;
+  },
+  down() {
+    this.step--;
+    return this;
+  },
+  showStep() {
+    console.log(this.step);
+    return this;
+  },
+};
+
+ladder.up().up().down().showStep().down().showStep(); // shows 1 then 0
