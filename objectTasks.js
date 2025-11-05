30e995a8a7a9665ff5fdc9b98a084286c0e38339
vetacode <vetacode@gmail.com>
2025-11-05 12:31:30 +0700

    task 4 object accomplished

--- objectTasks.js
+++ objectTasks.js
@@ -70,3 +70,63 @@ for (let key in salaries) {
 }
 
 console.log(sum);
+
+/*Task4
+Multiply numeric property values by 2
+importance: 3
+Create a function multiplyNumeric(obj) that multiplies all numeric property values of obj by 2.
+
+For instance:
+
+// before the call
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu"
+};
+
+multiplyNumeric(menu);
+
+// after the call
+menu = {
+  width: 400,
+  height: 600,
+  title: "My menu"
+};
+Please note that multiplyNumeric does not need to return anything. It should modify the object in-place.
+
+P.S. Use typeof to check for a number here.
+*/
+
+let menu = {
+  width: 200,
+  height: 300,
+  title: 'My menu',
+};
+console.log(menu);
+
+function multiplyNumeric(obj) {
+  for (let key in obj) {
+    if (typeof obj[key] === 'number') {
+      obj[key] *= 2;
+    }
+  }
+}
+
+console.log(multiplyNumeric(menu)); //return undefined coz the function is not returning anything
+
+//solution: run the function first then log the menu
+
+multiplyNumeric(menu);
+console.log(menu);
+
+//solution 2: return the obj on the function
+function multiplyNumeric(obj) {
+  for (let key in obj) {
+    if (typeof obj[key] === 'number') {
+      obj[key] *= 2;
+    }
+  }
+  return obj;
+}
+console.log(multiplyNumeric(menu)); // it will return the modified obj
