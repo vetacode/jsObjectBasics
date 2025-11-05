5334810dc1fd254405a103a9d7f48a75a489d8aa
vetacode <vetacode@gmail.com>
2025-11-05 17:56:20 +0700

    adding method basics

--- this.js
+++ this.js
@@ -10,3 +10,16 @@ user.sayHi = function () {
 user.sayHi(); // Hello!
 
 console.log(user);
+//A function that is a property of an object is called its method.
+//sayHiß is a function that is a property of user object
+//sayHi is a METHOD of the object user.
+
+let mobil = {};
+
+function terbang() {
+  console.log('ga bisa donk!');
+}
+
+mobil.terbang = terbang; //masukin key terbang ke obj mobil dgn value function terbang ==> terbang adalah method
+
+mobil.terbang();
