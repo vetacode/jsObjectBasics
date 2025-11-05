58d39465c8e40186be7d06e11e9b90e319c64b5b
vetacode <vetacode@gmail.com>
2025-11-05 22:40:07 +0700

    task 2 accomplished

--- this.js
+++ this.js
@@ -146,3 +146,37 @@ function makeUser2() {
 }
 let user7 = makeUser2();
 console.log(user7.ref().name); //John
+
+/*Task2
+Create a calculator
+importance: 5
+Create an object calculator with three methods:
+
+read() prompts for two values and saves them as object properties with names a and b respectively.
+sum() returns the sum of saved values.
+mul() multiplies saved values and returns the result.
+let calculator = {
+  // ... your code ...
+};
+
+calculator.read();
+alert( calculator.sum() );
+alert( calculator.mul() );
+*/
+
+let calculator = {
+  read() {
+    this.a = 2;
+    this.b = 4;
+  },
+  sum() {
+    return this.a + this.b;
+  },
+  mul() {
+    return this.a * this.b;
+  },
+};
+
+calculator.read();
+console.log(calculator.sum());
+console.log(calculator.mul());
