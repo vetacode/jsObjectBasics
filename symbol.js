6363c093a29f2902264102d8606a360997c90853
vetacode <vetacode@gmail.com>
2025-11-06 14:58:01 +0700

    symbol skipped by for in loops

--- symbol.js
+++ symbol.js
@@ -7,3 +7,28 @@ console.log(id.toString()); // Symbol(id), now it works
 
 let id3 = Symbol('id');
 console.log(id.description); // id
+
+//can create hidden props
+
+let user = {
+  // belongs to another code
+  name: 'John',
+};
+
+let id4 = Symbol('id');
+
+user[id4] = 1;
+console.log(user[id4]); // we can access the data using the symbol as the key
+
+//symbol doesnt work on for..in loops
+let id5 = Symbol('id');
+let user2 = {
+  name: 'John',
+  age: 30,
+  [id5]: 123,
+};
+
+for (let key in user2) console.log(key); // name, age (no symbols)
+
+// the direct access by the symbol works
+console.log('Direct: ' + user2[id5]); // Direct: 123
