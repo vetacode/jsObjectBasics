ae0f4fe736643ad2adf06f018dacc54e0d8f93ba
vetacode <vetacode@gmail.com>
2025-11-06 11:44:24 +0700

    task 1 accomplished

--- constructorNew.js
+++ constructorNew.js
@@ -19,3 +19,34 @@ console.log(aladin); //Alibaba, overwrites Ali that passed by aladin variable
 
 //we can omit parenthesis (tanda kurung) klo ga passing argumen. But this is not a best practices
 // let alibaba = new User;
+
+/* Task 1
+Two functions – one object
+importance: 2
+Is it possible to create functions A and B so that new A() == new B()?
+
+function A() { ... }
+function B() { ... }
+
+let a = new A();
+let b = new B();
+
+alert( a == b ); // true
+If it is, then provide an example of their code.
+*/
+
+function Heaven() {
+  this.name = 'Surga';
+  return { name: 'Neraka' };
+}
+
+function Hell() {
+  this.name = 'Neraka';
+}
+
+let nyaman = new Heaven();
+let success = new Hell();
+
+success = nyaman;
+
+console.log(nyaman == success);
