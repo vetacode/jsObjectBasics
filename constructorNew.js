a6a3829342548705cef414c3882c44e77a49753f
vetacode <vetacode@gmail.com>
2025-11-06 11:11:46 +0700

    adding return on constructor

--- constructorNew.js
+++ constructorNew.js
@@ -6,3 +6,13 @@ function User(name) {
 let visitor = new User('Alex');
 
 console.log(visitor);
+
+//if using return, will override the 'this'
+function User2(name) {
+  this.name = name;
+  this.admin = true;
+  return { name: 'Alibaba' };
+}
+
+let aladin = new User2('Ali');
+console.log(aladin); //Alibaba, overwrites Ali that passed by aladin variable
