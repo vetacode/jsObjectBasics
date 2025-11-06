b0846163b13e8f227b303ecef06614a6d101f91c
vetacode <vetacode@gmail.com>
2025-11-06 14:38:58 +0700

    add other variant of chaining

--- optionalChaining.js
+++ optionalChaining.js
@@ -15,3 +15,29 @@ let x = 0;
 
 user3?.sayHi(x++); // no "user", so the execution doesn't reach sayHi call and x++
 console.log(x); // 0, value not incremented
+
+//other variant of chaining: ?.() and ?.[]
+
+let userAdmin = {
+  admin() {
+    console.log('I am admin');
+  },
+};
+
+let userGuest = {};
+
+userAdmin.admin?.(); // I am admin
+
+userGuest.admin?.(); // nothing happens (no such method)
+
+let key = 'firstName';
+
+let user1 = {
+  firstName: 'John',
+};
+
+let user4 = null;
+console.log(user1?.[key]); // John
+console.log(user2?.[key]); // undefined
+
+delete user?.name; // delete user.name if user exists
