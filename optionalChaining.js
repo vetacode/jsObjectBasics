02bac85aa791e9bc00df3f4fd659fe8c34f8cb73
vetacode <vetacode@gmail.com>
2025-11-06 14:34:12 +0700

    add short circuiting

--- optionalChaining.js
+++ optionalChaining.js
@@ -7,4 +7,11 @@ let user2 = null;
 console.log(user?.address); // undefined
 console.log(user?.address?.street); // undefined
 
-//Please note: the ?. syntax makes optional the value before it, but not any further.
+//Please note: the ?. syntax makes optional the value BEFORE it, but not any further.
+
+//short-circuiting
+let user3 = null;
+let x = 0;
+
+user3?.sayHi(x++); // no "user", so the execution doesn't reach sayHi call and x++
+console.log(x); // 0, value not incremented
