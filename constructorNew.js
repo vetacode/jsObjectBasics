eacbd15dda6be088c2584238c150c6c7728796f3
vetacode <vetacode@gmail.com>
2025-11-06 12:08:02 +0700

    task 3 accomplished

--- constructorNew.js
+++ constructorNew.js
@@ -96,3 +96,37 @@ let result = new Calculator(2, 4);
 result.read();
 console.log(result.sum());
 console.log(result.mul());
+
+/*Tas 3
+Create new Accumulator
+importance: 5
+Create a constructor function Accumulator(startingValue).
+
+Object that it creates should:
+
+Store the “current value” in the property value. The starting value is set to the argument of the constructor startingValue.
+The read() method should use prompt to read a new number and add it to value.
+In other words, the value property is the sum of all user-entered values with the initial value startingValue.
+
+Here’s the demo of the code:
+
+let accumulator = new Accumulator(1); // initial value 1
+
+accumulator.read(); // adds the user-entered value
+accumulator.read(); // adds the user-entered value
+
+alert(accumulator.value); // shows the sum of these values
+*/
+
+function Accumulator(startingValue) {
+  this.value = startingValue;
+  this.read = function () {
+    this.value += 3;
+  };
+}
+
+let accumulator = new Accumulator(1);
+accumulator.read();
+accumulator.read();
+
+console.log(accumulator.value);
